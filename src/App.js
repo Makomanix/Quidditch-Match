bc6f7b9872392066f9b6c28d1b0d0d6f0c536786
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-10-26 17:32:19 -0400

    first posting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quidditch Rumble</h1>
+      <Navbar 
+        <nav>
+          <ul>
+            <li></li> 
+          </ul>
+        </nav>
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <main>
+        <p>Are you ready to RUMBLE?</p>
+      </main>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <main>
+        <h1>About</h1>
+        <p>
+          This application is collection of Harry Potter characters that you can
+          create a Quidditch team with
+        </p>
+      </main>
+    </>
   );
 }
 
