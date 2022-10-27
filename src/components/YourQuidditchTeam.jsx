e404c0c422c846eb8290a910c1ee3ed5dc88939d
Jonathan Bruckman <jbruckman86@gmail.com>
2022-10-27 17:42:17 -0400

    stuff and things

--- src/components/YourQuidditchTeam.jsx
+++ src/components/YourQuidditchTeam.jsx
@@ -6,8 +6,6 @@ function YourQudditchTeam({ character, onRemoveChar }) {
     if (character === []) {
         return null;
     }
-    //const [characterIndex, setCharacterIndex] = useState(7);
-    // above is for a "show more button"
 
     const characterCards = character.map((character) => {
         return (
