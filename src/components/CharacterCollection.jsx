d58967f3b3b0cbadab6125e8aea76e93eb283528
Geng-Wung <gengxis92@gmail.com>
2022-10-26 20:33:42 -0400

    filled in component code kinda

--- src/components/CharacterCollection.jsx
+++ src/components/CharacterCollection.jsx
@@ -3,16 +3,39 @@ import Character from "./Character";
 import Container from "react-bootstrap/Container";
 
 function CharacterCollection({
-    character,
-    searchQuery,
-    onAddTeam,
-    setSearchQuery,
+  character,
+  searchQuery,
+  onAddTeam,
+  setSearchQuery,
 }) {
-    const [characterIndex, setCharacterIndex] = useState(7);
-    const characterCards = characters
-        .slice(0, characterIndex)
-        .map((character) => {
-            return <Character key={character.id} character={character} />;
-        });
+
+  //const [characterIndex, setCharacterIndex] = useState(7);
+  // above is for a "show more button"
+  const characterCards = character.map((character) => {
+    return ( 
+      <Character 
+        key={character.id}
+        id={character.id} 
+        character={character}
+        onCharClick={onAddTeam} 
+      />
+      );
+    });
+
+  const handleOnChange = (e) => setSearchQuery(e.target.value);
+
+  return (
+    <div>
+        <div>{characterCards}</div>
+        <div>
+            <input 
+              type="text"
+              placeholder="search for character"
+              onChange={handleOnChange}
+              value={searchQuery} 
+            ></input>
+        </div>
+    </div>
+  );
 }
 export default CharacterCollection;
---
+++ src/components/YourQuidditchTeam.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Character from './Character';
+
+function YourQudditchTeam({character, onRemoveChar}) {
+  if (character === []) {
+    return null;
+  }
+    //const [characterIndex, setCharacterIndex] = useState(7);
+    // above is for a "show more button"
+
+    const characterCards = character.map((character) => {
+      return ( 
+        <Character 
+          key={character.id}
+          id={character.id} 
+          character={character}
+          onCharClick={onRemoveChar}
+        />
+        );
+      });
+  
+    
+  
+    return (
+      <div>
+          <div>{characterCards}</div>
+          <div>
+              <input 
+                type="text"
+                placeholder="search for character"
+                onChange={handleOnChange}
+              ></input>
+          </div>
+      </div>
+    );
+  }
+  export default YourQudditchTeam;
